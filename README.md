de46214d52675df67c49da0c708fd40be5683d7f
Danielle Walker <danielle@bliss.net.au>
2019-02-07 01:53:00 +1100

    Started working on reading CSV file

--- README.md
+++ README.md
@@ -8,10 +8,12 @@
 * Ubuntu Server 16.04.5 LTS (Xenial Xerus)   
 * PHP 7.2   
 * PostgreSQL 9.5
+* OpenSSH
 
 #### Assumptions:
-* PHP should use Postgre library and use vanilla SQL
-* An ORM library shouldn't be used for the Db.
-* As the program is to run via Bash, its assumed this would be an 'Inhouse App'.
+* CSV file will be located in the same folder as the PHP Script
+* CSV file will contain data in the structured order of: Name, Surname, Email
+* CSV file may contain white spaces in the header.  Either trim the header before creating the key for an Associative Array or use an Indexed.
+* CSV file may contain non Alpha characters in Name & Surname.  All non-alpha characters will be removed, thus assuming no hyphenated will exist or be allowed.
 
 #### Instructions:
--- user_upload.php
+++ user_upload.php
@@ -1,9 +1,13 @@
 <?php
+  //TODO: change echo statements to stdout.
+  //    **CSV file contains white spaces in header.  Assuming this will exist when Catalyst run it, then either the header needs to be trimmed or change it to an indexed array.
+  //    futher filtering on name/surname to remove non alpha characters.
+  //    Database handling
 
   //Handle the directives from the Bash input
   foreach ($argv as $value)
   {
-    //Simple check if any directives were received.  If not, display error & exit.
+    //Simple check if any directives were received.  If not, display error, help instructions & exit.
     if (sizeof($argv) <= 1) {
       echo ("Error Command.  Missing Directive input");
       exit(helpInstructions());
@@ -13,6 +17,14 @@
     switch ($argv[1])
     {
       case "--file":
+        //check to see if a filename was added as a directive, and that the file exists
+        if ((empty($argv[2])) OR (checkFileExists($argv[2]) == false))
+        {
+          exit ("Unknown command $argv[1].  File name missing or File Not Found\n");
+        } else {
+          readCSVFile($argv[2]);
+          break;
+        }
 
       case "--create-table":
 
@@ -26,10 +38,42 @@
         exit(helpInstructions());
 
       default:
-        exit ("Unkown command $argv[1].  Try the --help directive for assistance\n");
+        exit ("Unknown command $argv[1].  Try the --help directive for assistance\n");
       }
     }
 
+    //Simple function to check if a specified file exists
+    function checkFileExists($fileName)
+    {
+      if (!file_exists($fileName))
+        {
+          return false;
+        } else {
+          return true;
+        }
+    }
+
+    //function used to read data from a CSV file into an array.  The "checkFileExists" function should always be run prior to this, to check the file exists.
+    function readCSVFile($fileName)
+    {
+      $rows = array_map('str_getcsv', file($fileName));
+      print_r($rows);             //For testing purposes
+      $head = array_shift($rows);
+
+      $csvfile = array();
+      $x = 0;
+      foreach ($rows as $row) {
+        $csvfile[] = array_combine($head, $row);
+        $csvfile[$x]['name'] = ucfirst(strtolower($csvfile[$x]['name']));
+        $csvfile[$x]['surname'] = ucfirst(strtolower($csvfile[$x]['surname']));
+        $x++;
+      }
+      echo ("USER: ".ucfirst(strtolower($csvfile[6]['name']))."\n");     //For testing purposes
+      echo ("USER: ".ucfirst(strtolower($csvfile[6]['surname']))."\n");  //For testing purposes
+      //echo ("USER: ".ucfirst(strtolower($csvfile[6]['email']))."\n");
+    }
+
+    //Simple function to display help instructions
     function helpInstructions()
     {
       echo "\n     |>>** USER_UPLOAD DIRECTIVES **<<|     \n";
