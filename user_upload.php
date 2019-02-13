9ff2ef640f8395377e7385383c456bca497cc1ac
Danielle Walker <danielle@bliss.net.au>
2019-02-13 20:36:18 +1100

    Fixed duplicate CSV data + checking if dbase table exists

--- user_upload.php
+++ user_upload.php
@@ -1,7 +1,11 @@
 <?php
-  //TODO: futher filtering on name/surname to remove non alpha characters.
-  //      Database handling
-  //      Add a check that Primary Key (email) doesn't already exist in database
+
+  //check if any directives were entered after the program name
+  if (sizeof($argv) <= 1)
+  {
+    fwrite(STDOUT, "Error in Command.  Missing Directive instruction\n");
+    exit(helpInstructions());
+  }
 
   //Check if the XML file for settings exist, if not create it, then load the contents into a new object;
   if (checkFileExists("dbconnect.xml") == false)
@@ -10,13 +14,6 @@
   }
   $xml = new stdClass();
   $xml = simplexml_load_file("dbconnect.xml");
-
-  //check if any directives were entered after the program name
-  if (sizeof($argv) <= 1)
-  {
-    fwrite(STDOUT, "Error Command.  Missing Directive input\n");
-    exit(helpInstructions());
-  }
 
   //Handle Bash directive arguments
   switch ($argv[1])
@@ -31,20 +28,27 @@
         try {
           $db = new Dbase();
           $pgsql = $db->dbConnect($xml->host, $xml->port, $xml->database, $xml->username, $xml->password);
-          $data = readCSVFile($argv[2]);
+          //if (!empty($db->createTable($pgsql)))
+          //{
+          //  fwrite(STDOUT, "Users Table Created Successfully\n");
+            //pg_close($pgsql);
+          //}
+          $data = readCSVFile($argv[2]);                                            //Read the CSV file
           if ($db->insertData($pgsql, $data[0]))
           {
-            fwrite(STDOUT, "Successfully added data to the Database\n");
             pg_close($pgsql);
+            createErrorIssue($data[1], $data[2]);                                   //create the Error & Issues CSV files.
+            fwrite(STDOUT, "Successfully added records to the database\n");
+            fwrite(STDOUT, "Check the files Error_log.csv and Issues_log.csv for any problems with the data.\n");
           }
-          createErrorIssue($data[1], $data[2]);                               //create the Error & Issues CSV files.
         } catch (Exception $e) {
-          fwrite(STDOUT,"ERROR: ".$e->getMessage()." \n");
+          pg_close($pgsql);
+          fwrite(STDOUT,"Error with Database: ".$e->getMessage()." \n");
         }
       } else {
         $data = readCSVFile($argv[2]);
         createErrorIssue($data[1], $data[2]);
-        fwrite(STDOUT, "Successfully performed Dry Run on CSV data\n");
+        fwrite(STDOUT, "Successfully performed Dry Run on CSV data.  Check Error_log.csv and Issues_log.csv for any problems with the data.\n");
       }
       break;
 
@@ -53,13 +57,18 @@
       try {
         $db = new Dbase();
         $pgsql = $db->dbConnect($xml->host, $xml->port, $xml->database, $xml->username, $xml->password);
-        if ($db->createTable($pgsql))
+        if ($db->checkUserTableExists($pgsql) == false)
         {
-          fwrite(STDOUT, "Table Create Successfully\n");
-          pg_close($pgsql);
+          if ($db->createTable($pgsql))
+          {
+            fwrite(STDOUT, "Users Table Created Successfully\n");
+          }
+        } else {
+          fwrite(STDOUT, "Users Table already Exists\n");
         }
+        pg_close($pgsql);
       } catch (Exception $e) {
-          fwrite(STDOUT,"ERROR: ".$e->getMessage()." \n");
+          fwrite(STDOUT,"Error Creating Table: ".$e->getMessage()." \n");
       }
       break;
 
@@ -74,7 +83,7 @@
           pg_close($pgsql);
         }
       } catch (Exception $e) {
-         fwrite(STDOUT,"ERROR: ".$e->getMessage()." \n");
+         fwrite(STDOUT,"Error Communicating with Database: ".$e->getMessage()." \n");
       }
       break;
 
@@ -189,33 +198,58 @@
     {
       $rows = array_map('str_getcsv', file($fileName));
       $head = array_shift($rows);
-      $trim_head = array_map('trim', $head);                                             //trim white space from header names
+      $trim_head = array_map('trim', $head);                                            //trim white space from header names
 
       $csvfile = array();                                                               //array of data to be written to database
-      $errlist = array();                                                               //array of data which couldn't be written to database due to invalid email or already existing email
+      $csvNoDup = array();                                                              //a copy of csvfile array with duplicates removed
+      $errlist = array();                                                               //array of data which couldn't be written to database due to invalid email or duplicate email
       $issuelist = array();                                                             //array of data which may need to be corrected in db, due to non-alpha charcters in name/surname
-      $x = 0;
+      $x = $y = 0;
+
       foreach ($rows as $row) {
         $csvfile[] = array_combine($trim_head, $row);
         $csvfile[$x]['name'] = ucfirst(strtolower(trim($csvfile[$x]['name'])));         //convert name to lowercase with first char Upper + trim whitespace
         $csvfile[$x]['surname'] = ucfirst(strtolower(trim($csvfile[$x]['surname'])));   //convert surname to lowercase with first char Upper + trim whitespace
         $csvfile[$x]['email'] = strtolower(trim($csvfile[$x]['email']));                //convert email to lowercase + trim whitespace
-        $z = $x;
 
         if (!filter_var($csvfile[$x]['email'], FILTER_VALIDATE_EMAIL))
         {
-          array_push($errlist, $csvfile[$x]);                                           //add invalid email record to ErrList Array
-          array_splice($csvfile[$x], $x, 1);                                                //remove invalid email record & reindex CSVFile array
+          array_push($errlist, $csvfile[$x]);                                           //add invalid email record (including name/surname) to ErrList Array
+          array_splice($csvfile[$x], $x, 1);                                            //remove invalid email record (including name/surname) & reindex CSVFile array
         }
-        if (!ctype_alpha($csvfile[$x]['name']) || (!ctype_alpha($csvfile[$z]['surname'])))
+        if (!ctype_alpha($csvfile[$x]['name']) || (!ctype_alpha($csvfile[$x]['surname'])))
         {
           array_push($issuelist, $csvfile[$x]);                                          //name or surname contains non-alpha characters, so add it to the Issues Array
         }
         $x++;
       }
+      $data = removeDuplicateValues($csvfile, 'email');
+      $csvfile = $data[0];                                                              //csvfile now contains non-duplicate data.
+
+      foreach ($data[1] as $col) {
+        array_push($errlist, $data[1][$y]);                                             //Add any duplicates found to the ErrList array which will appear in the error_log csv file.
+        $y++;
+      }
       return array($csvfile, $errlist, $issuelist);
-      //print_r($errlist);    //testing only
-      //print_r($csvfile);    //testing only
+    }
+
+    //function which removes duplicates from csvfile & returns a duplicate free csv array + error array
+    function removeDuplicateValues($array, $key)
+    {
+      $temparr = $keyarr = $errarr = $errkey = array();
+      $i = 0;
+
+      foreach($array as $val) {
+        if (!in_array($val[$key], $keyarr)) {
+          $keyarr[$i] = $val[$key];
+          $temparr[$i] = $val;
+        } else {
+          $errkey[$i] = $val[$key];
+          $errarr[$i] = $val;
+        }
+        $i++;
+      }
+      return array(array_values($temparr), array_values($errarr));                        //reindex both arrys & return them as 1 array
     }
 
     //function which creates CSV file for the Error_log and/or Issue_list
@@ -229,6 +263,7 @@
       fclose($csv);
     }
 
+    //function which checks if the error & issues array have values - and if they do, it creates the CSV file.
     function createErrorIssue($errlog, $issuelog)
     {
       if (!empty($errlog))
@@ -238,61 +273,6 @@
       if (!empty($issuelog))
       {
         createCSV("Issue_log", $issuelog);
-      }
-    }
-
-    class Dbase
-    {
-      //function which creates the initial connection to the Postgre Database
-      function dbConnect($host, $port, $dbname, $user, $password)
-      {
-        $pgsql = @pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password);
-        if ($pgsql)
-        {
-          return $pgsql;
-        }
-      }
-
-      //function which checks if the 'users' table exists
-      function checkTableExists($pg)
-      {
-        $query = "SELECT 1 from 'users' LIMIT 1";
-        $checktable = pg_query($pg, $query);
-        if ($checktable !== FALSE)
-        {
-          return true;
-        } else {
-          return false;
-        }
-      }
-
-      //function to create the 'users' table.
-      function createTable($pg)
-      {
-        $query = "CREATE TABLE IF NOT EXISTS users (
-                    email VARCHAR(50) PRIMARY KEY,
-                    name VARCHAR(25) NOT NULL,
-                    surname VARCHAR(25) NOT NULL)";
-        $createtable = pg_query($pg, $query);
-        if ($createtable)
-        {
-          return $createtable;
-        }
-      }
-
-      //function which inserts data to the 'users' table
-      function insertData($pg, $csvfile)
-      {
-        $x = 0;
-        foreach ($csvfile as $data)
-        {
-          $email = pg_escape_string($csvfile[$x]['email']);
-          $name = pg_escape_string($csvfile[$x]['name']);
-          $surname = pg_escape_string($csvfile[$x]['surname']);
-          $query = "INSERT INTO users (email, name, surname) VALUES ('".$email."', '".$name."', '".$surname."')";
-          $result = pg_query($pg, $query);
-          $x++;
-        }
       }
     }
 
@@ -323,5 +303,61 @@
     {
       throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
     }
+
+    class Dbase
+    {
+      //function which creates the initial connection to the Postgre Database
+      function dbConnect($host, $port, $dbname, $user, $password)
+      {
+        $pgsql = @pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password);
+        if ($pgsql)
+        {
+          return $pgsql;
+        }
+      }
+
+      //function to create the 'users' table.
+      function createTable($pg)
+      {
+        $query = "CREATE TABLE IF NOT EXISTS users (
+                    email VARCHAR(50) PRIMARY KEY,
+                    name VARCHAR(25) NOT NULL,
+                    surname VARCHAR(25) NOT NULL)";
+        $createtable = pg_query($pg, $query);
+        if ($createtable)
+        {
+          return $createtable;
+        }
+      }
+
+      //function to check the Users Table exists.  If it does return 'true' otherwise return 'false'
+      function checkUserTableExists($pg)
+      {
+        $query = "SELECT 1 FROM users LIMIT 1";
+        try {
+          if (@pg_query($pg, $query) != null) {
+            return true;
+          }
+        } catch (Exception $e) {
+          return false;
+        }
+      }
+
+      //function which inserts data to the 'users' table
+      function insertData($pg, $csvfile)
+      {
+        $x = 0;
+        foreach ($csvfile as $data)
+        {
+          $email = pg_escape_string($csvfile[$x]['email']);
+          $name = pg_escape_string($csvfile[$x]['name']);
+          $surname = pg_escape_string($csvfile[$x]['surname']);
+          $query = "INSERT INTO users (email, name, surname) VALUES ('".$email."', '".$name."', '".$surname."')";
+          $result = pg_query($pg, $query);
+          $x++;
+        }
+        return $result;
+      }
+    }  //End Class
 
   ?>
